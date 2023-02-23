e82506aca01d07f04a354f908b87252821d980da
Bharath <bharath.sudarsanam04@gmail.com>
2023-02-23 19:35:17 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,165 @@
+const logotext = "B4R";
+const meta = {
+    title: "B4R",
+    description: "I’m Bharath Sudarsanam API developer _ Front End Developer,currently studying in India",
+}
+
+const introdata = {
+    title: "I’m Bharath Sudarsanam",
+    animated: {
+        first: "I love coding",
+        second: "I code amazing shit",
+        third: "Founder @TarsAi",
+        forth: "#DOGETOTHEMOON",
+    },
+    description: "Developer, Designer & Freelancer. I embrace challenges and ain't afraid to break the mold. Let's link up to change the game and make some history!",
+    your_img_url: "https://i.ibb.co/h2HS0rY/My-project-1-21.png",
+};
+
+const dataabout = {
+    title: "Bout Me",
+    aboutme: "Yo, what's up fam! I'm Bharath, I live for the thrill of coding and keeping up with the tech industry. When I'm not coding up a storm, you can find me geekin' out over the latest tech trends. I'm a person who is convinced that the only thing that drove me on was the love for the work i did!! I believe I'm at a point in my life where my ultimate goal is to succeed in learning something new till the end... more like to be a jack of all trades. Physics freak & Coding geek #choosesustainable",
+};
+const worktimeline = [{
+        jobtitle: "Lab Coordiantor",
+        where: "Naresh IT",
+        date: "Oct 2021 - Nov 2021",
+    },
+    {
+        jobtitle: "Web Developer Intern",
+        where: "TXON",
+        date: "Oct 2022 - Dec 2022",
+    },
+    {
+        jobtitle: "Technical and Product Dev Intern",
+        where: "MLRIT CIE",
+        date: "Jul 2022 - Present",
+    },
+    {
+        jobtitle: "Founder",
+        where: "Tars Ai",
+        date: "Feb 2023",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Core Java",
+        value: 80,
+    },
+    {
+        name: "Javascript",
+        value: 60,
+    },
+    {
+        name: "React",
+        value: 75,
+    },
+    {
+        name: "CPP/C",
+        value: 85,
+    },
+    {
+        name: "Selenium WebDriver",
+        value: 70,
+    },
+];
+
+
+
+
+
+const services = [{
+        title: "UI & UX Design",
+        description: "UI design focuses on the visual and interactive elements of a digital product, while UX design encompasses the entire user experience, from research to testing, to ensure a seamless and satisfying user journey",
+    },
+    {
+        title: "API Development",
+        description: "API development involves creating a set of protocols and tools for building software applications, allowing communication between different systems and enabling data exchange between them.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "WordPress design involves customizing the visual appearance and functionality of a website built with the WordPress content management system, using themes, plugins, and other design elements to create a unique and user-friendly online presence.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://i.ibb.co/HnKXd1b/image-1.png",
+        desctiption: "the design refers to the visual and interactive elements of an e-commerce platform.",
+        link: "https://www.figma.com/file/5p8bxpeDqrvw2BO8I28jHC/TARSy2j?node-id=0%3A1&t=qQBpr3wSj4F9KIkn-1",
+    },
+    {
+        img: "https://i.ibb.co/C0Hk0dv/Safeimagekit-resized-img-5.png",
+        desctiption: "a software interface that allows developers to retrieve up-to-date stock market data",
+        link: "https://github.com/Bharath-tars/STOCK_INFLATION-NEWS_API",
+    },
+    {
+        img: "https://i.ibb.co/vQVBLTZ/image-5.png",
+        desctiption: "a tool that can automatically apply to job openings on the LinkedIn platform.",
+        link: "https://github.com/Bharath-tars/Automating_LinkedIn_JobApplications",
+    },
+    {
+        img: "https://i.ibb.co/685p3fz/Safeimagekit-resized-img-10.png",
+        desctiption: "an automated system designed to send personalized birthday greetings.",
+        link: "https://github.com/Bharath-tars/Mail-Bot",
+    },
+    {
+        img: "https://i.ibb.co/pbLV5K3/Safeimagekit-resized-img-7.png",
+        desctiption: "a powerful tool for developers looking to enhance their messaging applications with real-time weather data and forecasting capabilities.",
+        link: "https://github.com/Bharath-tars/WEATHER_MESSAGE_API",
+    },
+    {
+        img: "https://i.ibb.co/pvrHR8b/image-3.png",
+        desctiption: "a model which analyzes large datasets of patient information to identify patterns and correlations between different factors and the likelihood of developing breast cancer",
+        link: "https://jupyter.org/try-jupyter/retro/notebooks/?path=data/Breast%20Cancer%20Prediction%20Using%20SVM.ipynb",
+    },
+    {
+        img: "https://i.ibb.co/7jJcHBm/image-2.png",
+        desctiption: "an algorithm that allows users to track the International Space Station (ISS)",
+        link: "https://github.com/Bharath-tars/ISS-NOTIFIER-API",
+    },
+    {
+        img: "https://i.ibb.co/fXp2DXR/Safeimagekit-resized-img-11.png",
+        desctiption: "a bot designed to automate certain tasks, provide helpful utilities, and enhance the overall functionality and user experience of Discord.",
+        link: "https://github.com/Bharath-tars/Discord_Memer_Bot",
+    },
+    {
+        img: "https://i.ibb.co/kxGwKy8/Safeimagekit-resized-img-12.png",
+        desctiption: "a software system that utilizes artificial intelligence and machine learning algorithms to estimate the value of a house based on various factors.",
+        link: "https://github.com/Bharath-tars/Predicting_House_Prices",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "bharathworks2u@gmail.com",
+    YOUR_FONE: "+91 8639196174",
+    description: "I'm hell - bent about making a real difference... let's catch up!! -> Try using the hyperlink above if the message box keeps loading.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_50ruluq",
+    YOUR_TEMPLATE_ID: "template_krfskpm",
+    YOUR_USER_ID: "bharath",
+};
+
+const socialprofils = {
+    github: "https://github.com/Bharath-tars",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/bharath-s-81b4bb1a4/",
+    twitter: "https://twitter.com/itzmebharath300",
+    Codepen: "https://codepen.io/bharath-tars",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
